9daffcac8414786966647d36bd58252c932e3d4e
asad-au-ullah <asad.au.ullah@gmail.com>
2025-11-28 17:36:11 +0500

    Implement Random Color Generator

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,16 @@
-import './App.css'
-import Accordian from './components/accordian'
+import "./App.css";
+import Accordian from "./components/accordian";
+import RandomColor from "./components/random-color";
 
 function App() {
-
   return (
     <>
-      <Accordian />
+      {/* Accordian */}
+      {/* <Accordian /> */}
+      {/* Random Color Generator */}
+      <RandomColor />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
