28b64b5c84de3feb01658eed244016ebca404c46
asad-au-ullah <asad.au.ullah@gmail.com>
2025-11-29 20:57:49 +0500

    Image Slider / Carousal implemented

--- src/App.jsx
+++ src/App.jsx
@@ -2,16 +2,26 @@ import "./App.css";
 import Accordian from "./components/accordian";
 import RandomColor from "./components/random-color";
 import StarRating from "./components/star-rating";
+import ImageSlider from "./components/image-slider";
 
 function App() {
   return (
     <>
       {/* Accordian */}
       {/* <Accordian /> */}
+
       {/* Random Color Generator */}
       {/* <RandomColor /> */}
+
       {/* Star Rating */}
-      <StarRating noOfStars={10} />
+      {/* <StarRating noOfStars={10} /> */}
+
+      {/* Image Slider */}
+      <ImageSlider
+        url={"https://picsum.photos/v2/list"}
+        page={"1"}
+        limit={"10"}
+      />
     </>
   );
 }
